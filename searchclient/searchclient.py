bdb0e255106332a364a4b4778683713efef1cbe5
Nicolai <nicolai.verbaarschot@gmail.com>
2023-04-19 21:19:06 +0200

    Initial implementation of CBS. We will need to modify 'search' to search for a single agent. We will also have to change how we are handling frontiers; Their return values among other things.

--- searchclient/searchclient.py
+++ searchclient/searchclient.py
@@ -2,6 +2,8 @@ import argparse
 import io
 import sys
 import time
+from typing import TypedDict
+import copy
 
 import cProfile
 
@@ -11,6 +13,7 @@ from state import State
 from frontier import FrontierBFS, FrontierDFS, FrontierBestFirst
 from heuristic import HeuristicAStar, HeuristicWeightedAStar, HeuristicGreedy
 from graphsearch import search
+from action import Action
 
 class SearchClient:
     @staticmethod
@@ -19,11 +22,11 @@ class SearchClient:
         # Read domain.
         server_messages.readline() # #domain
         server_messages.readline() # hospital
-        
+
         # Read Level name.
         server_messages.readline() # #levelname
         server_messages.readline() # <name>
-        
+
         # Read colors.
         server_messages.readline() # #colors
         agent_colors = [None for _ in range(10)]
@@ -39,7 +42,7 @@ class SearchClient:
                 elif 'A' <= e <= 'Z':
                     box_colors[ord(e) - ord('A')] = color
             line = server_messages.readline()
-        
+
         # Read initial state.
         # line is currently "#initial".
         num_rows = 0
@@ -68,11 +71,11 @@ class SearchClient:
                     boxes[row][col] = c
                 elif c == '+':
                     walls[row][col] = True
-            
+
             row += 1
         del agent_rows[num_agents:]
         del agent_rows[num_agents:]
-        
+
         # Read goal state.
         # line is currently "#goal".
         goals = [['' for _ in range(num_cols)] for _ in range(num_rows)]
@@ -82,20 +85,20 @@ class SearchClient:
             for col, c in enumerate(line):
                 if '0' <= c <= '9' or 'A' <= c <= 'Z':
                     goals[row][col] = c
-            
+
             row += 1
             line = server_messages.readline()
-        
+
         # End.
         # line is currently "#end".
-        
+
         State.agent_colors = agent_colors
         State.walls = walls
         State.box_colors = box_colors
         State.goals = goals
         return State(agent_rows, agent_cols, boxes)
 
-    
+
     @staticmethod
     def print_search_status(start_time: 'int', explored: '{State, ...}', frontier: 'Frontier') -> None:
         status_template = '#Expanded: {:8,}, #Frontier: {:8,}, #Generated: {:8,}, Time: {:3.3f} s\n[Alloc: {:4.2f} MB, MaxAlloc: {:4.2f} MB]'
@@ -106,70 +109,112 @@ class SearchClient:
     def main(args) -> None:
         # Use stderr to print to the console.
         print('SearchClient initializing. I am sending this using the error output stream.', file=sys.stderr, flush=True)
-        
+
         # Send client name to server.
         if hasattr(sys.stdout, "reconfigure"):
             sys.stdout.reconfigure(encoding='ASCII')
         print('SearchClient', flush=True)
-        
+
         # We can also print comments to stdout by prefixing with a #.
         print('#This is a comment.', flush=True)
-        
+
         # Parse the level.
         server_messages = sys.stdin
         if hasattr(server_messages, "reconfigure"):
             server_messages.reconfigure(encoding='ASCII')
         initial_state = SearchClient.parse_level(server_messages)
-        
-        # Select search strategy.
-        frontier = None
-        if args.bfs:
-            frontier = FrontierBFS()
-        elif args.dfs:
-            frontier = FrontierDFS()
-        elif args.astar:
-            frontier = FrontierBestFirst(HeuristicAStar(initial_state))
-        elif args.wastar is not False:
-            frontier = FrontierBestFirst(HeuristicWeightedAStar(initial_state, args.wastar))
-        elif args.greedy:
+
+        if args.naive:
+            # Select search strategy.
+            frontier = None
+            if args.bfs:
+                frontier = FrontierBFS()
+            elif args.dfs:
+                frontier = FrontierDFS()
+            elif args.astar:
+                frontier = FrontierBestFirst(HeuristicAStar(initial_state))
+            elif args.wastar is not False:
+                frontier = FrontierBestFirst(HeuristicWeightedAStar(initial_state, args.wastar))
+            elif args.greedy:
+                frontier = FrontierBestFirst(HeuristicGreedy(initial_state))
+            else:
+                # Default to BFS search.
+                frontier = FrontierBFS()
+                print('Defaulting to BFS search. Use arguments -bfs, -dfs, -astar, -wastar, or -greedy to set the search strategy.', file=sys.stderr, flush=True)
+
+
+            # Search for a plan.
+            print('Starting {}.'.format(frontier.get_name()), file=sys.stderr, flush=True)
+            plan = search(initial_state, frontier)
+
+            # Print plan to server.
+            if plan is None:
+                print('Unable to solve level.', file=sys.stderr, flush=True)
+                sys.exit(0)
+            else:
+                print('Found solution of length {}.'.format(len(plan)), file=sys.stderr, flush=True)
+
+                for joint_action in plan:
+                    print("|".join(a.name_ for a in joint_action), flush=True)
+                    # We must read the server's response to not fill up the stdin buffer and block the server.
+                    response = server_messages.readline()
+
+        #CBS
+        class Root(TypedDict):
+            solution: list[list[Action]] | None
+            constraints: list[tuple[int, int, int]]
+            cost: float
+
+        def cost(solution: list[list[Action]]| None):
+            if solution is None:
+                return float("inf");
+            cost = 0.0
+            for agent_solution in solution:
+                cost += len(agent_solution)
+            return cost
+
+        if args.cbs:
+            root: Root = { "solution": [], "constraints": [], "cost": 0 }
+            for agent, _ in enumerate(initial_state.agent_rows):
+                root["solution"] = search(initial_state, agent)
+            root["cost"] = cost(root["solution"])
             frontier = FrontierBestFirst(HeuristicGreedy(initial_state))
-        else:
-            # Default to BFS search.
-            frontier = FrontierBFS()
-            print('Defaulting to BFS search. Use arguments -bfs, -dfs, -astar, -wastar, or -greedy to set the search strategy.', file=sys.stderr, flush=True)
-        
-        # Search for a plan.
-        print('Starting {}.'.format(frontier.get_name()), file=sys.stderr, flush=True)
-        plan = search(initial_state, frontier)
-        
-        # Print plan to server.
-        if plan is None:
-            print('Unable to solve level.', file=sys.stderr, flush=True)
-            sys.exit(0)
-        else:
-            print('Found solution of length {}.'.format(len(plan)), file=sys.stderr, flush=True)
-            
-            for joint_action in plan:
-                print("|".join(a.name_ for a in joint_action), flush=True)
-                # We must read the server's response to not fill up the stdin buffer and block the server.
-                response = server_messages.readline()
+            while not frontier.is_empty:
+                node = frontier.pop()
+                if node.is_goal_state():
+                    return node["solution"] # need to adapt returned object of frontier queue to contain solution, i.e Root type.
+                a1, a2, v, t = FindFirstConflict(node["solution"])
+                for a in [a1, a2]:
+                    m = copy.deepcopy(node)
+                    m["constraints"].append((a, v, t))
+                    m["solution"][a] = search(initial_State[a], m.constraints)
+                    m["cost"] = cost(m["solution"])
+                    if m["cost"] < float("inf"):
+                        frontier.add(m)
+
+
+
 
 if __name__ == '__main__':
     # Program arguments.
     parser = argparse.ArgumentParser(description='Simple client based on state-space graph search.')
     parser.add_argument('--max-memory', metavar='<MB>', type=float, default=2048.0, help='The maximum memory usage allowed in MB (soft limit, default 2048).')
-    
+
     strategy_group = parser.add_mutually_exclusive_group()
     strategy_group.add_argument('-bfs', action='store_true', dest='bfs', help='Use the BFS strategy.')
     strategy_group.add_argument('-dfs', action='store_true', dest='dfs', help='Use the DFS strategy.')
     strategy_group.add_argument('-astar', action='store_true', dest='astar', help='Use the A* strategy.')
     strategy_group.add_argument('-wastar', action='store', dest='wastar', nargs='?', type=int, default=False, const=5, help='Use the WA* strategy.')
     strategy_group.add_argument('-greedy', action='store_true', dest='greedy', help='Use the Greedy strategy.')
-    
+    highlevel_strategy_group = parser.add_mutually_exclusive_group()
+    highlevel_strategy_group.add_argument('-naive', action='store_true', dest='naive', help='Use the Naive strategy.')
+    highlevel_strategy_group.add_argument('-cbs', action='store_true', dest='cbs', help='Use the CBS strategy.')
+
+
     args = parser.parse_args()
-    
+
     # Set max memory usage allowed (soft limit).
     memory.max_usage = args.max_memory
-    
+
     # Run client.
     SearchClient.main(args)
