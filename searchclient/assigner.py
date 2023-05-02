6f6c94e6ad34b5198c134e0a1f235bad3a2d725f
samyhaff <haffoudhisamy@gmail.com>
2023-05-02 21:07:28 +0200

    progress on task assignment

--- searchclient/assigner.py
+++ searchclient/assigner.py
@@ -1,15 +1,14 @@
-import heapq
-from math import floor
-from state import State
+import collections
 from sys import stderr
 
+from state import State
 
-class FloodFill:
+class PathUtils:
     def __init__(self, grid):
         self.grid = grid
         self.reachability_map = [[False] * len(grid[0]) for _ in range(len(grid))]
 
-    def dfs(self, x, y):
+    def dfs(self, x, y) -> None:
         if x < 0 or y < 0 or x >= len(self.grid) or y >= len(self.grid[0]):
             return
         if self.grid[x][y]:
@@ -22,11 +21,41 @@ class FloodFill:
         self.dfs(x, y + 1)
         self.dfs(x, y - 1)
 
-    def get_reachability_map(self, position):
+    def bfs(self, start, target):
+        queue = collections.deque([[start]])
+        seen = set([start])
+
+        while queue:
+            path = queue.popleft()
+            y, x = path[-1]
+
+            if (y, x) == target:
+                return path
+
+            for y2, x2 in ((y + 1, x), (y - 1, x), (y, x + 1), (y, x - 1)):
+                if 0 <= x2 < len(self.grid[0]) and 0 <= y2 < len(self.grid) and not self.grid[y2][x2] and (y2, x2) not in seen:
+                    queue.append(path + [(y2, x2)])
+                    seen.add((y2, x2))
+
+        return None
+
+    def get_reachability_map(self, position) -> list:
         self.reachability_map = [[False] * len(self.grid[0]) for _ in range(len(self.grid))]
         x, y = position
         self.dfs(x, y)
         return self.reachability_map
+
+    # def extract_goals(self, path) -> list:
+    #     goals_encountered = []
+    #     for x, y in path:
+    #         if self.goals[y][x] != '':
+    #             goals_encountered.append(self.goals[y][x])
+    #     return goals_encountered
+    #
+    # def get_goals_on_shortest_path(self, start, target) -> list:
+    #     path = self.bfs(start, target)
+    #     goals_encountered = self.extract_goals(path)
+    #     return goals_encountered
 
 
 class Goal:
@@ -46,7 +75,7 @@ class Assigner:
         n_rows, n_cols = len(State.goals), len(State.goals[0])
         agent_color = State.agent_colors[agent]
         letters = [chr(ord('A') + i) for i in range(26) if State.box_colors[i] == agent_color]
-        floodfill = FloodFill(State.walls)
+        floodfill = PathUtils(State.walls)
         reachability_map = floodfill.get_reachability_map((self.state.agent_rows[agent], self.state.agent_cols[agent]))
         boxes = {}
         for letter in letters:
@@ -59,7 +88,7 @@ class Assigner:
         # print(f"Agent {agent} goals: {goals}", file=stderr, flush=True)
         return boxes, goals
 
-    def build_initial_graph(self, agent):
+    def build_initial_graph(self, agent) -> dict:
         tasks = self.assign_tasks_to_agent(agent)[1]
         graph = dict()
         start = Goal("start", None)
@@ -73,5 +102,15 @@ class Assigner:
                 graph[start].add(goal)
         return graph
 
+    def get_graph(self, agent) -> dict:
+        graph = self.build_initial_graph(agent)
+        agent_position = (self.state.agent_rows[agent], self.state.agent_cols[agent])
+        path_utils = PathUtils(State.walls)
+        print(graph, file=stderr, flush=True)
+        for node, neighbours in graph.items():
+            path = path_utils.bfs(agent_position, node.position)
+            print(f"Agent {agent} path: {path}", file=stderr, flush=True)
+        return graph
+
     def assign_plans(self) -> dict:
-        return [self.build_initial_graph(agent) for agent in range(len(self.state.agent_rows))]
+        return [self.get_graph(agent) for agent in range(len(self.state.agent_rows))]
