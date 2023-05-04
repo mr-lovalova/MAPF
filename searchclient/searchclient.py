892a29c59b222900bd35ec0338d7655edb1395ba
mr-lovalova <47424670+mr-lovalova@users.noreply.github.com>
2023-05-04 21:44:47 +0200

    ,,

--- searchclient/searchclient.py
+++ searchclient/searchclient.py
@@ -95,8 +95,6 @@ class SearchClient:
         State.agent_colors = agent_colors
         State.walls = walls
         State.box_colors = box_colors
-        # Conflict.constraints = [None for _ in range(num_agents)]
-        # Conflict.resolveable = [True for _ in range(num_agents)]
         print(box_colors, file=sys.stderr)
         # State.goals = goals
         return State(agent_rows, agent_cols, boxes, goals)
